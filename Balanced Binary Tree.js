2a37ead4a7a289b77cb0ce9c75341ed55b22bd6d
Parvez Kose <flamboyanz@gmail.com>
2019-10-18 21:10:13 -0400

    bst update

--- Balanced Binary Tree.js
+++ Balanced Binary Tree.js
@@ -10,38 +10,42 @@
  * @return {boolean}
  */
 
-const Height = node => {
-    if (node == null)
-        return 0;
-    else {
-        let heightLeft = Height(node.left);
-        let heightRight = Height(node.right)
-        if (heightLeft > heightRight)
-            return heightLeft + 1;
-        else
-            return heightRight + 1;
-    }
-}
+class BST {
+  constructor() {}
 
-const Difference = node => {
- 
-    return Math.abs(Height(node.left) - Height(node.right))
+  Height = node => {
+    if (node == null) return 0;
+    else {
+      let heightLeft = this.Height(node.left);
+      let heightRight = this.Height(node.right);
+      if (heightLeft > heightRight) return heightLeft + 1;
+      else return heightRight + 1;
+    }
+  };
+
+  Difference = node => {
+    return Math.abs(this.Height(node.left) - this.Height(node.right));
+  };
+
+  isBalanced = root => {
+    if (root == null) return true;
+    // balanceFactor
+
+    if (root.left != null && this.Difference(root.left) > 1) return false;
+    if (root.right != null && this.Difference(root.right) > 1) return false;
+
+    if (!this.isBalanced(root.left) || !this.isBalanced(root.right))
+      return false;
+
+    if (this.Difference(root) > 1) return false;
+
+    return true;
+  };
 }
 
 var isBalanced = function(root) {
-    if (root == null) return true;
-    // balanceFactor
-    
-    if (root.left != null && Difference(root.left) > 1) return false;
-    if (root.right != null && Difference(root.right) > 1) return false;
-    
-    if (!isBalanced(root.left) || !isBalanced(root.right))
-        return false;
-    
-    if (Difference(root) > 1)
-        return false;
-    
-    return true;
+  const bst = new BST();
+  return bst.isBalanced(root);
 };
 
 /*
