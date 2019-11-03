ce1a18f41951e9ac85af6fb3b8f072b1a7ea4af0
Parvez Kose <flamboyanz@gmail.com>
2019-11-03 11:00:20 -0500

    find given sum

---
+++ OA/find-pair-with-given-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let HashMap = new Map();
+
+  for (let i = 0; i < nums.length; ++i) {
+    let compliment = target - nums[i];
+    if (HashMap.has(compliment)) return [HashMap.get(compliment), i];
+    else HashMap.set(nums[i], i);
+  }
+
+  return [];
+};
