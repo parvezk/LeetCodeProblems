986ec4210ef80d1e2b00972d77bdc76c6064d11e
Parvez Kose <flamboyanz@gmail.com>
2019-11-05 23:42:08 -0500

    keys and rooms

---
+++ 841-keys-and-rooms.js
@@ -0,0 +1,25 @@
+var canVisitAllRooms = function(rooms) {
+  
+    const visited = new Set([0]);
+    let stack = new Stack(rooms[0]);
+    
+    while (!stack.isEmpty())
+    {
+      const curr = stack.pop();
+      
+      for (let i=0; i<curr.length; ++i)
+      {
+        const keyList = rooms[curr[i]];
+        if (!visited.has(curr[i]))
+        {
+          if (keyList.length > 0) 
+            stack.push(keyList)
+  
+          visited.add(curr[i]);
+        }
+      }
+    }
+    //console.log(visited, 'finished')
+   return visited.size == rooms.length
+  };
+  
