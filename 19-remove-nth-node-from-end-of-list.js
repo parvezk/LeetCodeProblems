5d5bc09261d37149e2a86aca121a70d400314427
Parvez Kose <flamboyanz@gmail.com>
2019-11-10 20:07:12 -0500

    remove nth node

---
+++ 19-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let count = 0;
+    let curr = head;
+    
+    while (curr) {
+        count++;
+        curr = curr.next;
+    }
+
+    let prev = null;
+    curr = head;
+    while (curr) {
+        if (count == n) {
+            if (prev)
+                prev.next = curr ? curr.next : null
+            else
+                head = head.next;
+            return head;
+        }
+        count--;
+        prev = curr;
+        curr = curr.next;
+    }
+    
+    return null;
+};
