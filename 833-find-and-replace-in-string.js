9857707bb796b79671b4749451391429bfaf4ce0
Parvez Kose <flamboyanz@gmail.com>
2020-05-06 12:30:00 -0300

    find and replace strings
    update
    
    update

---
+++ 833-find-and-replace-in-string.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} S
+ * @param {number[]} indexes
+ * @param {string[]} sources
+ * @param {string[]} targets
+ * @return {string}
+ */
+
+var findReplaceString = function(S, indexes, sources, targets) {
+    let N = S.length;
+    let match = new Array(N).fill(-1);
+    
+    for (let i=0; i<indexes.length; ++i) {
+        let ix = indexes[i];
+        if (S.substring(ix, ix + sources[i].length) == sources[i])
+            match[ix] = i;
+    }
+
+    let ans = '';
+    let ix = 0;
+    while (ix < N) {
+        if (match[ix] >= 0) {
+            ans += targets[match[ix]];
+            ix += sources[match[ix]].length;
+        } else {
+            ans += S.charAt(ix++);
+        }
+    }
+    return ans;
+};
