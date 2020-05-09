420ed3f8e2248f25ed8c5a5ff53232519f4c3ab5
Parvez Kose <flamboyanz@gmail.com>
2020-05-09 12:30:00 -0300

    license key formating
    update

---
+++ 482-license-key-formatting.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var licenseKeyFormatting = function (S, K) {
+    if (S.length == 1 && !S.includes('-'))
+        return S.toUpperCase();
+
+    S = S.replace(/-/ig, '');
+    let ans = '',
+        mod = S.length % K;
+
+    if (mod) {
+        ans += S.slice(0, mod) + '-';
+        S = S.slice(mod);
+    }
+
+    for (let i = 0; i < S.length; i = i + K) {
+        if (i != 0) ans += '-';
+        ans += S.substring(i, i + K);
+    }
+    return ans.toUpperCase();
+};
