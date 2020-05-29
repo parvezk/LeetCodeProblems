0282c1d6c0350d4fb52fc4378b93f29a5b073467
Parvez Kose <flamboyanz@gmail.com>
2020-05-28 23:17:14 -0400

    update

--- 95-unique-binary-search-trees-ii.js
+++ 95-unique-binary-search-trees-ii.js
@@ -22,20 +22,20 @@ var generateTrees = function(n) {
       
       // pick up a root
       for (let i = start; i <= end; i++) {
-      // all possible left subtrees if i is choosen to be a root
-      let left_trees = generate_trees(start, i-1);
-      // all possible right subtrees if i is choosen to be a root
-      let right_trees = generate_trees(i+1, end);
-      
-      // connect left and right trees to the root i
-      for (l of left_trees) {
-        for (r of right_trees) {
-          let current_tree = new TreeNode(i);
-          current_tree.left = l;
-          current_tree.right = r;
-          all_trees.push(current_tree);
+        // all possible left subtrees if i is choosen to be a root
+        let left_trees = generate_trees(start, i-1);
+        // all possible right subtrees if i is choosen to be a root
+        let right_trees = generate_trees(i+1, end);
+        
+        // connect left and right trees to the root i
+        for (l of left_trees) {
+            for (r of right_trees) {
+            let current_tree = new TreeNode(i);
+            current_tree.left = l;
+            current_tree.right = r;
+            all_trees.push(current_tree);
+            }
         }
-      }
       }
       return all_trees;
     }
