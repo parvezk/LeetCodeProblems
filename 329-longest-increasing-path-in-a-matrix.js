490201fc1db56eadf3cd2d9aa3fe7eb53e79bdb0
Parvez Kose <flamboyanz@gmail.com>
2020-05-29 00:02:48 -0400

    update lc

--- 329-longest-increasing-path-in-a-matrix.js
+++ 329-longest-increasing-path-in-a-matrix.js
@@ -2,31 +2,32 @@
  * @param {number[][]} matrix
  * @return {number}
  */
+// DP / DFS + Memoization
 var longestIncreasingPath = function(matrix) {
-    if (matrix.length == 0) return 0;
-    let m = matrix.length, n = matrix[0].length;
+  if (!matrix.length) return 0;
+  
+  let m = matrix.length, n = matrix[0].length,
+      dirs = [[0,1], [1,0], [0,-1], [-1,0]],
+      cache = new Array(m).fill(null).map(() => new Array(n).fill(0)),
+      ans = 0;
+  
+  const dfs = (matrix, i, j) => {
+    if (cache[i][j]) return cache[i][j];
     
-    const dfs = (matrix, i, j, cached) => {
-      if (cached[i][j] != 0) return cached[i][j];
-      
-        for (let d of dirs) {
-          let x = i + d[0], y = j + d[1];
-          if (0 <= x && x < m && 0 <= y && y < n && matrix[x][y] > matrix[i][j])
-            cached[i][j] = Math.max(cached[i][j], dfs(matrix, x, y, cached));
-        }
-      
-      cached[i][j]++;
-      return cached[i][j];
-    }
+      for (let d of dirs) {
+        let x = i + d[0], y = j + d[1];
+
+        if (x >= 0 && x < m && y >= 0 && y < n && matrix[x][y] > matrix[i][j])
+          cache[i][j] = Math.max(cache[i][j], dfs(matrix, x, y));
+      }
     
-    let dirs = [[0,1], [1,0], [0,-1], [-1,0]];
-    let cache = new Array(m).fill(null).map(() => new Array(n).fill(0));
-    let ans = 0;
-    
-    for (let i = 0; i < m; i++) {
-      for (let j = 0; j < n; j++)
-        ans = Math.max(ans, dfs(matrix, i, j, cache));
-    }
-    
-    return ans;
-  };
+    cache[i][j]++;
+    return cache[i][j]
+  }
+  
+  for (let i = 0; i < m; i++)
+    for (let j = 0; j < n; j++)
+      ans = Math.max(ans, dfs(matrix, i, j));
+  
+  return ans;
+};
