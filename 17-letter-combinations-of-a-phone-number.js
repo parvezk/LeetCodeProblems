952ceea764098b8971c87902a7695471cd05b3b3
Parvez Kose <flamboyanz@gmail.com>
2020-06-03 12:40:00 -0300

    letter combo
    update

--- 17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number.js
@@ -2,27 +2,25 @@
  * @param {string} digits
  * @return {string[]}
  */
-
 var letterCombinations = function(digits) {
+  if (digits.length == 0) return [];
   
-    const mapping = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'],
-          list = [];
-    
-    if (digits.length == 0) return list;
-    
-    const helper = (curr, index) => {
+  const mapping = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'],
+        combinations = [];
   
-      if (index == digits.length)
-        list.push(curr)
-      else {
-        let pos = Number.parseInt(digits.charAt(index)),
-            candidates = mapping[pos];
+  const helper = (curr = '', index = 0) => {
+    if (index == digits.length)
+      combinations.push(curr);
+    else {
+      const digit = digits.charAt(index),
+            candidates = mapping[+digit];
+      
+      for (let i = 0; i < candidates.length; i++)
+        helper(curr + candidates.slice(i, i + 1), index + 1);
+    }
+  } 
   
-        for (let i = 0; i < candidates.length; i++)
-          helper(curr + candidates.substring(i, i + 1), index + 1);
-      }
-    };
-    
-    helper('', 0);
-    return list;
-  };
+  helper();
+  return combinations;
+  
+};
