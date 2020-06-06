8b742a9476a146e9ae6d4a16089a3f97c1a3cabe
Parvez Kose <flamboyanz@gmail.com>
2020-06-05 23:43:20 -0400

    cracking safe

--- 753-cracking-the-safe.js
+++ 753-cracking-the-safe.js
@@ -4,30 +4,26 @@
  * @return {string}
  */
 var crackSafe = function(n, k) {
-    if (n == 1 && k == 1) return "0";
+  if (n == 1 && k == 1) return '0';
+  
+  let seen = new Set(), ans = '',
+      start = '0'.repeat(n - 1);
+  
+  const dfs = (node, k) => {
     
-    let seen = new Set(),
-        ans = '';
-    
-    let sb = '';
-    for (let i = 0; i < n - 1; i++)
-      sb += '0';
-    
-    let start = sb;
-    
-    const dfs = (node, k) => {
-      for (let x = 0; x < k; x++) {
-        let nei = node + x;
+    for (let i = 0; i < k; i++) {
+      let nei = node + i;
+      
+      if (!seen.has(nei)) {
+        seen.add(nei);
         
-        if (!seen.has(nei)) {
-          seen.add(nei);
-          dfs(nei.substring(1), k);
-          ans += x;
-        }
+        dfs(nei.substring(1), k);
+        ans += i;
       }
     }
-    
-    dfs(start, k);
-    ans += start;
-    return ans;
-  };
+  }
+  
+  dfs(start, k);
+  ans += start;
+  return ans;
+};
