96fe08a2adcbe27476ef8a44729d81fd34c3a25e
Parvez Kose <flamboyanz@gmail.com>
2020-06-05 23:43:03 -0400

    max subarray

--- 53-maximum-subarray.js
+++ 53-maximum-subarray.js
@@ -1,25 +1,35 @@
-// Using Greedy approach
-// Pick the locally optimal move at each step,
-// and that will lead to the globally optimal solution.
-var maxSubArray = function(nums) {
-  let curSum = nums[0],
-    maxSum = curSum;
-
-  for (let i = 1; i < nums.length; i++) {
-    curSum = Math.max(nums[i], curSum + nums[i]); // local max
-    maxSum = Math.max(maxSum, curSum); // global max
-  }
-
-  return maxSum;
-};
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 
 // DP approach
 var maxSubArray = function(nums) {
-  let max_Sum = nums[0];
-
+  let curr_sum = nums[0];
+  
   for (let i = 1; i < nums.length; i++) {
-    if (nums[i - 1] > 0) nums[i] += nums[i - 1];
-    max_Sum = Math.max(max_Sum, nums[i]);
+    nums[i] = Math.max(nums[i], curr_sum + nums[i])
   }
-  return max_Sum;
+  let max_seen = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    max_seen = Math.max(max_seen, nums[i]);
+    nums[i] = max_seen;
+  }
+  
+  return nums[nums.length - 1];
+}
+  
+
+// Using Greedy approach
+// Pick the locally optimal move at each step, 
+// and that will lead to the globally optimal solution.
+var maxSubArray = function(nums) {
+  let curSum = nums[0], maxSum = curSum;
+  
+  for (let i = 1; i < nums.length; i++) {
+    curSum = Math.max(nums[i], curSum + nums[i]) // local max
+    maxSum = Math.max(maxSum, curSum);  // global max
+  }
+  
+  return maxSum;
 };
