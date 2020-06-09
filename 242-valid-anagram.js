28e1179c53641e0ff8e73c6936b8e591358600de
Parvez Kose <flamboyanz@gmail.com>
2020-06-09 12:40:00 -0300

    valid anagram
    update

--- 242-valid-anagram.js
+++ 242-valid-anagram.js
@@ -4,53 +4,36 @@
  * @return {boolean}
  */
 
-// Sorting one line
-var isAnagram = function(s, t) {
-  return (
-    s
-      .split("")
-      .sort()
-      .join("") ==
-    t
-      .split("")
-      .sort()
-      .join("")
-  );
-};
-
 // Sorting approach
 var isAnagram = function(s, t) {
-  let s1 = s
-      .split("")
-      .sort()
-      .join(""),
-    t1 = t
-      .split("")
-      .sort()
-      .join("");
-
-  return s1 == t1;
+  return (s.split("").sort().join("") == t.split("").sort().join(""));
 };
 
 // Hashmap approach
 var isAnagram = function(s, t) {
   const map = new Map();
-
+  let c;
+  
   for (let i = 0; i < s.length; i++) {
-    let char = s.charAt(i);
-    if (map.has(char)) map.set(char, map.get(char) + 1);
-    else map.set(char, 1);
+    c = s.charAt(i);
+    
+    if (map.has(c))
+      map.set(c, map.get(c) + 1);
+    else
+      map.set(c, 1);
   }
-
+  
   for (let j = 0; j < t.length; j++) {
-    let char = t.charAt(j);
-    if (map.has(char)) {
-      if (map.get(char) <= 1) map.delete(char);
-      else map.set(char, map.get(char) - 1);
-    } else {
-      map.set(char, 1);
-    }
+    c = t.charAt(j);
+    
+    if (map.has(c)) {
+      if (map.get(c) > 1)
+        map.set(c, map.get(c) - 1);
+      else
+        map.delete(c)
+    } else
+      map.set(c, 1);
   }
-
-  return map.size <= 0 ? true : false;
+  //return map.size <= 0 ? true : false;
+  return map.size == 0;
 };
