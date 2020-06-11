f0536f4604e15108db0e36627227eab9336fa139
Parvez Kose <flamboyanz@gmail.com>
2020-06-11 13:32:35 -0400

    merge interval

--- 56-merge-intervals.js
+++ 56-merge-intervals.js
@@ -5,15 +5,15 @@
 
 var merge = function(intervals) {
   intervals.sort((a, b) => a[0] - b[0]);
-  let output = intervals[0] ? [intervals[0]] : []; // merged times
+  let output = intervals[0] ? [intervals.shift()] : []; // merged times
 
-  for (let i = 1; i < intervals.length; i++) {
-    let current = intervals[i],
-      lastMerged = output[output.length - 1];
-    if (current[0] <= lastMerged[1]) {
-      //lastMerged[1] = Math.max(interval[1], lastMerged[1]);
-      lastMerged[1] = current[1] >= lastMerged[1] ? current[1] : lastMerged[1];
-    } else output.push(current);
+  for (let interval of intervals) {
+    const last = output[output.length - 1];
+    if (interval[0] <= last[1])
+      //last[1] = Math.max(interval[1], last[1]);
+      last[1] = last[1] > interval[1] ? last[1] : interval[1];
+    else 
+      output.push(interval);
   }
   return output;
 };
