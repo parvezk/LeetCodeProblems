eb1665a01960944b0737f01d5f4edabcdfc55367
Parvez Kose <flamboyanz@gmail.com>
2020-06-16 23:38:39 -0400

    lru cache

--- 146-lru-cache.js
+++ 146-lru-cache.js
@@ -2,45 +2,46 @@
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-  this.HashMap = new Map();
+  this.cache = new Map();
   this.capacity = capacity;
 };
 
-/**
+/** 
  * @param {number} key
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-  if (this.HashMap.has(key)) {
-    let values = this.HashMap.get(key);
-    this.HashMap.delete(key);
-    this.HashMap.set(key, values);
+  if (this.cache.has(key)) {
+    const values = this.cache.get(key);
+    this.cache.delete(key)
+    this.cache.set(key, values);
     return values[values.length - 1];
-  } else return -1;
+  }
+  else
+    return -1;
 };
 
-/**
- * @param {number} key
+/** 
+ * @param {number} key 
  * @param {number} value
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-  if (this.HashMap.has(key)) {
-    let values = this.HashMap.get(key);
-    values.push(value);
-    this.HashMap.delete(key);
-    this.HashMap.set(key, values);
+  if (this.cache.has(key)) {
+    const values = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, [...values, value]);
   } else {
-    if (this.HashMap.size >= this.capacity) {
-      let least = this.HashMap.keys().next().value;
-      this.HashMap.delete(least);
+    if (this.cache.size >= this.capacity) {
+      const least = this.cache.keys().next().value;
+      this.cache.delete(least);
     }
-
-    this.HashMap.set(key, [value]);
+    
+    this.cache.set(key, [value]);
   }
 };
 
-/**
+/** 
  * Your LRUCache object will be instantiated and called as such:
  * var obj = new LRUCache(capacity)
  * var param_1 = obj.get(key)
---
+++ 410-split-array-largest-sum.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} nums
+ * @param {number} m
+ * @return {number}
+ */
+
+//Binary Search + Greedy
+var splitArray = function(nums, m) {
+    let l = 0, r = 0, n = nums.length;
+    
+    for (let i = 0; i < n; i++) {
+      r += nums[i];
+      if (l < nums[i]) l = nums[i];
+    }
+    let ans = r;
+    
+    while (l <= r) {
+      let mid = Math.trunc((l + r) / 2);
+      let sum = 0, cnt = 1;
+      
+      for (let i = 0; i < n; i++) {
+        if (sum + nums[i] > mid) {
+          cnt++;
+          sum = nums[i];
+        } else {
+          sum += nums[i];
+        }
+      }
+      
+      if (cnt <= m) {
+        ans = Math.min(ans, mid);
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return ans;
+  };
+  
+  // Using Dynamic Programming 
+  var splitArray = function(nums, m) {
+    let n = nums.length,
+        f = new Array(n + 1).fill(null).map(
+          () => new Array(m + 1).fill(Number.MAX_VALUE)),
+        sub = new Array(n + 1).fill(0);
+    
+    for (let i = 0; i < n; i++)
+      sub[i + 1] = sub[i] + nums[i];
+    
+    f[0][0] = 0;
+    for (let i = 1; i <= n; i++) {
+      for (let j = 1; j <= m; j++) {
+        for (let k = 0; k < i; k++)
+          f[i][j] = Math.min(f[i][j], Math.max(f[k][j - 1], sub[i] - sub[k]));
+      }
+    }
+    return f[n][m]
+  };
