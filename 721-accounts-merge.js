b316e71193cd3ae3dfc7a6c4da69db15ec532beb
Parvez Kose <flamboyanz@gmail.com>
2020-06-29 12:40:00 -0300

    account merge
    update

---
+++ 721-accounts-merge.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function(accounts) {
+    const em_to_name = {},
+    graph = new Map();
+
+for (let acc of accounts) {
+  let name = acc[0];
+  for (let email of acc.slice(1)) {
+    
+    if (graph.has(acc[1])) graph.get(acc[1]).add(email);
+    else graph.set(acc[1], new Set([email]));
+    
+    if (graph.has(email)) graph.get(email).add(acc[1]);
+    else graph.set(email, new Set([acc[1]]));
+    
+    em_to_name[email] = name;
+  }
+}
+
+let seen = new Set(), ans = [];
+
+for (let email of graph.keys()) {
+  if (!seen.has(email)) {
+    seen.add(email);
+    let stack = [email], component = [];
+    
+    while (stack.length) {
+      let node = stack.pop();
+      component.push(node);
+      
+      for (let nei of graph.get(node)) {
+        if (!seen.has(nei)) {
+          seen.add(nei);
+          stack.push(nei);
+        }
+      }
+    } 
+    ans.push([em_to_name[email], ...component.sort()])
+  }
+}
+return ans;
+};
