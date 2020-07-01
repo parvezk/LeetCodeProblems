2276f66d6251cde681c95aa1d23d149c365c6c74
Parvez Kose <flamboyanz@gmail.com>
2020-07-01 17:05:47 -0400

    mim area rect

--- 939-minimum-area-rectangle.js
+++ 939-minimum-area-rectangle.js
@@ -2,29 +2,27 @@
  * @param {number[][]} points
  * @return {number}
  */
-var minAreaRect = function (points) {
+var minAreaRect = function(points) {
   let grid = new Set();
-
   for (let point of points)
     grid.add(point[0] + ',' + point[1]);
-
+  
   let ans = Number.MAX_SAFE_INTEGER;
-  const lastx = new Map();
-
+  
   for (let i = 0; i < points.length; i++) {
     for (let j = i + 1; j < points.length; j++) {
-
-      let p1 = points[i], p2 = points[j];
-      let x1 = p1[0], y1 = p1[1], x2 = p2[0], y2 = p2[1];
-
+      
+       let p1 = points[i], p2 = points[j],
+           x1 = p1[0], y1 = p1[1], x2 = p2[0], y2 = p2[1];
+      
       if (x1 != x2 && y1 != y2) {
         if (grid.has(x1 + ',' + y2) && grid.has(x2 + ',' + y1)) {
-          let area = Math.abs(y2 - y1) * Math.abs(x2 - x1);
-          ans = Math.min(ans, area);
+          let areaRect = Math.abs(y2 - y1) * Math.abs(x2 - x1);
+          
+          ans = Math.min(ans, areaRect);
         }
       }
     }
   }
-
   return ans == Number.MAX_SAFE_INTEGER ? 0 : ans;
 };
