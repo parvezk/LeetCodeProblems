b189c2ac156bda5c31c05668ea6a63751c401d7e
parvezk <flamboyanz@gmail.com>
2020-07-21 20:31:16 -0400

    linked list

--- 141-linked-list-cycle.js
+++ 141-linked-list-cycle.js
@@ -1,6 +1,4 @@
 /**
- * 141. Linked List Cycle
-
  * Definition for singly-linked list.
  * function ListNode(val) {
  *     this.val = val;
@@ -12,16 +10,17 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-
 var hasCycle = function(head) {
-  if (head == null || head.next == null) return false;
-
-  let slow = head;
-  let fast = head.next;
-
+  if (head == null || head.next == null)
+    return false;
+  
+  let slow = head,
+      fast = head.next;
+  
   while (slow != fast) {
-    if (fast == null || fast.next == null) return false;
-
+    if (slow == null || fast.next == null)
+      return false;
+    
     slow = slow.next;
     fast = fast.next.next;
   }
@@ -32,23 +31,19 @@ var hasCycle = function(head) {
 // Therefore, the worst case time complexity is O(N+K) = O(n)
 // Space complexity : O(1). We only use two nodes (slow and fast)
 
-/*
-// My method 
+// My method
 var hasCycle = function(head) {
-    if (head == null || head.next == null)
-        return false
-    
-    let slowRunner = head;
-    let fastRunner = head.next;
-    
-    while (slowRunner && fastRunner) {
-        
-        if (slowRunner == fastRunner)
-            return true;
-    
-        slowRunner = slowRunner.next || null;
-        fastRunner = fastRunner.next ? fastRunner.next.next :null;
-    }
+  if (head == null || head.next == null)
     return false;
-};
-*/
+  
+  let slow = head,
+      fast = head.next;
+  
+  while (slow && fast) {
+    if (slow == fast) return true;
+    
+    slow = slow.next || null
+    fast = fast.next ? fast.next.next : null;
+  }
+  return false;
+}
