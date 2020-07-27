1bb32dbdeee0f9643fd41fb78683c3028d6ce413
parvezk <flamboyanz@gmail.com>
2020-07-27 09:15:15 -0400

    arrays

--- 2-add-two-numbers.js
+++ 2-add-two-numbers.js
@@ -11,24 +11,23 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
+  let sum = 0, carry = 0,
+      preHead = new ListNode(-1, null),
+      curr = preHead;
+  
+  while (l1 || l2) {
+
+    sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    curr.next = new ListNode(sum % 10);
+    carry = Math.trunc(sum / 10);
     
-    let preHead = new ListNode(-1),
-        curr = preHead,
-        sum = 0, carry = 0;
-    
-    preHead.next = curr;
-    while (l1 || l2) {
-      
-      sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
-      curr.next = new ListNode(sum % 10);
-      carry = Math.trunc(sum / 10);
-      
-      l1 = l1 ? l1.next : null;
-      l2 = l2 ? l2.next : null;
-      
-      curr = curr.next;
-    }
-    
-    if (carry > 0) curr.next = new ListNode(carry);
-    return preHead.next;
-  };
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+    curr = curr.next;
+  }
+  
+  if (carry > 0) 
+    curr.next = new ListNode(carry);
+  
+  return preHead.next;
+};
