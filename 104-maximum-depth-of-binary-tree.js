4fea93e989307349e09cef59065080df1e177d3a
Parvez Kose <flamboyanz@gmail.com>
2020-08-05 00:01:17 -0400

    bst

--- 104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree.js
@@ -9,17 +9,6 @@
  * @param {TreeNode} root
  * @return {number}
  */
-
-// TOP DOWN APPROACH / Determine the parameters to help the node
-//find its answer; use the parameters and node value to determine
-//what values to be passed down to the childrens (to help them in turn)
-
-var maxDepth = function(root) {
-  if (root == null) return 0;
-
-  return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
-};
-
 // BOTTOM UP APPROACH / Propagate the answer up
 /*
 var maxDepth = function(root) {
@@ -30,3 +19,11 @@ var maxDepth = function(root) {
   return Math.max(height_left, height_right) + 1;
 };
 */
+
+// TOP DOWN APPROACH / Determine the parameters to help the node find its answer; use the parameters and node value to determine what values to be passed down to the childrens (to help find their answers themselves)
+
+var maxDepth = function(root) {
+  if (root == null) return 0;
+  
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
