b611f3c22520aa1c9f2cc97fcaba9b72cc91f8ea
Parvez Kose <flamboyanz@gmail.com>
2020-08-11 12:49:20 -0400

    tree

--- 116-populating-next-right-pointers-in-each-node.js
+++ 116-populating-next-right-pointers-in-each-node.js
@@ -7,38 +7,34 @@
  *    this.next = next === undefined ? null : next;
  * };
  */
+
 /**
  * @param {Node} root
  * @return {Node}
  */
-
-// Using level-order traversals
 var connect = function(root) {
   if (root == null) return null;
-
+  
   const levels = [];
-
+  
   const levelOrder = (node, level) => {
-    if (levels.length == level) levels.push([node]);
-    else levels[level].push(node);
-
-    if (node.left) levelOrder(node.left, level + 1);
-    if (node.right) levelOrder(node.right, level + 1);
-  };
-
-  levelOrder(root, 0);
-
-  for (level of levels) {
-    if (level.length == 1) continue;
-
-    for (let i = 0; i < level.length; i++) {
-      if (level[i + 1]) level[i].next = level[i + 1];
-    }
+    if (levels.length == level)
+      levels.push([node]);
+    else
+      levels[level].push(node)
+      
+      if (node.left) levelOrder(node.left, level + 1);
+      if (node.right) levelOrder(node.right, level + 1);
   }
-
+  
+  levelOrder(root, 0);
+  
+  for (let level of levels) {
+    if (level.length == 1) continue;
+    
+    for (let i = 0; i < level.length - 1; i++)
+        level[i].next = level[i + 1]
+  }
+  
   return levels[0][0];
 };
-
-// TODO
-// Using previously established next pointers
-var connect = function(root) {};
--- 43-multiply-strings.js
+++ 43-multiply-strings.js
@@ -5,21 +5,19 @@
  */
 // https://leetcode.com/problems/multiply-strings/discuss/17605/Easiest-JAVA-Solution-with-Graph-Explanation
 var multiply = function(num1, num2) {
-    let m = num1.length, n = num2.length;
-    let pos = new Array(m + n).fill(0);
-    
-    for (let i = m - 1; i >= 0; i--) {
-      for (let j = n - 1; j >= 0; j--) {
-        let mul = parseInt(num1.charAt(i)) * parseInt(num2.charAt(j));      
-        let p1 = i + j, p2 = i + j + 1;
-        let sum = mul + pos[p2];
-        
-        pos[p1] += parseInt(sum / 10);
-        pos[p2] = (sum) % 10;
-      }
+  let m = num1.length, n = num2.length,
+      val = new Array(m + n).fill(0);
+  
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      let mul = num1.charAt(i) * num2.charAt(j),
+          sum = mul + val[i + j + 1];
+      
+      val[i + j] += Math.trunc(sum / 10);
+      val[i + j + 1] = sum % 10;
     }
-    
-    let sb = '';
-    for (let p of pos) if (!(sb.length == 0 && p == 0)) sb += p;
-    return sb.length == 0 ? '0' : sb.toString();
-  };
+  }
+  // skip the leading zeros
+  while (val[0] === 0) val.shift();
+  return val.length ? val.join('') : '0';
+};
