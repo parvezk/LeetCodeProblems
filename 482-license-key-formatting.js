a9db7aa350b0582c8a0c470fd937de9e4232f6a4
Parvez Kose <flamboyanz@gmail.com>
2020-08-11 12:48:44 -0400

    tree

--- 482-license-key-formatting.js
+++ 482-license-key-formatting.js
@@ -3,22 +3,23 @@
  * @param {number} K
  * @return {string}
  */
-var licenseKeyFormatting = function (S, K) {
+var licenseKeyFormatting = function(S, K) {
     if (S.length == 1 && !S.includes('-'))
         return S.toUpperCase();
-
+    
     S = S.replace(/-/ig, '');
+    
     let ans = '',
         mod = S.length % K;
-
+    
     if (mod) {
-        ans += S.slice(0, mod) + '-';
-        S = S.slice(mod);
+      ans += S.slice(0, mod) + '-';
+      S = S.slice(mod);
     }
-
+    
     for (let i = 0; i < S.length; i = i + K) {
-        if (i != 0) ans += '-';
-        ans += S.substring(i, i + K);
+      if (i != 0) ans += '-';
+      ans += S.substring(i, i + K);
     }
     return ans.toUpperCase();
-};
+  };
