e1f326e9d4c751ce6420cebeef6d9d77cf7f49ae
Parvez Kose <flamboyanz@gmail.com>
2020-09-08 12:40:00 -0300

    remove duplicate
    remove dup

--- 26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array.js
@@ -2,22 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
+var removeDuplicates = nums => nums.splice(0, nums.length, ...new Set(nums)).length;
+
 var removeDuplicates = function(nums) {
   if (nums.length == 0) return 0;
+  
   let i = 0;
-  for (let j = 1; j < nums.length; ++j) {
+  for (let j = 1; j < nums.length; j++) {
     if (nums[i] != nums[j]) {
       i++;
       nums[i] = nums[j];
     }
   }
+  
   return i + 1;
 };
-
-/*
-[1,1,2]
-[0,0,1,1,1,2,2,3,3,4]
-[1,2,3]
-[]
-[1,1,1,1]
-*/
