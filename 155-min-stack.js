d60611ab686fdacb9943ff918620bf03a47e3d0c
Parvez Kose <flamboyanz@gmail.com>
2020-09-07 23:30:13 -0400

    update min stack

--- 155-min-stack.js
+++ 155-min-stack.js
@@ -1,45 +1,25 @@
 /**
  * initialize your data structure here.
  */
-var MinStack = function() {
-    this.stack = [];
-  };
+class MinStack {
+  _stack = [];
+
+  _last = (arr = this._stack) => (arr[arr.length - 1]);
+
+  push = (x) => {
+    if (this._stack.length == 0) {
+      this._stack.push([x, x]);
+      return;
+    }
+    
+    const currentMin = this._last()[1];
+    this._stack.push([x, Math.min(currentMin, x)]);
+    
+  }
   
-  /** 
-   * @param {number} x
-   * @return {void}
-   */
-  MinStack.prototype.push = function(x) {
-    this.stack.push(x);
-  };
-  
-  /**
-   * @return {void}
-   */
-  MinStack.prototype.pop = function() {
-    if (this.stack.length) return this.stack.pop();
-    else return null;
-  };
-  
-  /**
-   * @return {number}
-   */
-  MinStack.prototype.top = function() {
-    return this.stack[this.stack.length - 1];
-  };
-  
-  /**
-   * @return {number}
-   */
-  MinStack.prototype.getMin = function() {
-    return Math.min(...this.stack);
-  };
-  
-  /** 
-   * Your MinStack object will be instantiated and called as such:
-   * var obj = new MinStack()
-   * obj.push(x)
-   * obj.pop()
-   * var param_3 = obj.top()
-   * var param_4 = obj.getMin()
-   */
+  pop = () => this._stack.pop();
+
+  top = () => this._last()[0];
+
+  getMin = () => this._last()[1];
+}
