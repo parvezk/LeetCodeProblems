7bc633d7d3b6ea49ad55b8a6518b187d95e2715f
Parvez Kose <flamboyanz@gmail.com>
2020-11-19 12:30:00 -0300

    container with most water
    update date

--- 11-container-with-most-water.js
+++ 11-container-with-most-water.js
@@ -2,6 +2,7 @@
  * @param {number[]} height
  * @return {number}
  */
+// Two pointer approach - SELF - PROUD OF IT
 var maxArea = function(height) {
   let left = 0,
     right = height.length - 1,
@@ -17,4 +18,19 @@ var maxArea = function(height) {
     }
   }
   return ans;
+};
+
+// Two pointer - Modified
+var maxArea = function(height) {
+  let maxarea = 0,
+    l = 0,
+    r = height.length - 1;
+
+  while (l < r) {
+    maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+
+    if (height[l] < height[r]) l++;
+    else r--;
+  }
+  return maxarea;
 };
